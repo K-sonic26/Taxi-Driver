9a69f3e32f2ed682227189c7a090df8f250d4f05
Maysun Campos <96445806+raz810@users.noreply.github.com>
2022-10-13 22:00:42 -0600

    DRIVE!!!

---
+++ main.php
@@ -0,0 +1,16 @@
+<?php
+
+$drive = 10;
+$create_html = echo '<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8"/>
+<title>Project</title>
+</head>
+<body dir="ltr">
+<main>
+<div class="content">
+</div>
+</main>
+</body>
+</html>';
