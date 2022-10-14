61308918386b64c27635d080cde5cee368debe94
Geoumwa <115768691+Geoumwa@users.noreply.github.com>
2022-10-13 22:03:43 -0600

    Get Payment

--- check.php
+++ check.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+Written By: Maysun Campos
+And By: Geoumwa
+Written In: PHP
+Copyright Taxi Driver & Contributors 2022 All Rights Reserved
+*/
+
 $error = 404;
 $file = use main.php
 $unsuccessful;
